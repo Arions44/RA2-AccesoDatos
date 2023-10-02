db4155806144a39e24ae9252224564ed867f892a
Adrian Centeno <adriancentenosmr@gmail.com>
2023-10-02 20:04:55 +0200

    adrian

--- TrabajoRA2_Grupo6/src/test/Test.java
+++ TrabajoRA2_Grupo6/src/test/Test.java
@@ -4,5 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Test Raul");
+		
+		
+		System.out.println("hola caracola");
+
 	}
 }
