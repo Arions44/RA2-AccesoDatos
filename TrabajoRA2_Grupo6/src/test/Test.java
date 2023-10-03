5f38aac6c26ac8a47638ef8cefc1b4fb005699c5
Adrian Centeno <adriancentenosmr@gmail.com>
2023-10-03 18:58:56 +0200

    product update

--- TrabajoRA2_Grupo6/src/test/Test.java
+++ TrabajoRA2_Grupo6/src/test/Test.java
@@ -3,8 +3,8 @@ package test;
 import services.AzureSql;
 
 public class Test {
-	public static void main(String[] args) {
-		//AzureSql.ConnectingDB();
-		AzureSql.SelectProducts();
-	}
+    public static void main(String[] args) {
+        //AzureSql.ConnectingDB();
+        AzureSql.SelectProducts();
+    }
 }
