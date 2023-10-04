8c201cb0c49b5f02e18b809aba51973be3f77d1c
Adrian Centeno <adriancentenosmr@gmail.com>
2023-10-04 11:29:50 +0200

    model interfaces

---
+++ TrabajoRA2_Grupo6/src/views/InterfaceModel.java
@@ -0,0 +1,15 @@
+package views;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class InterfaceModel {
+	
+	public static void FrameModel(JFrame jf) {
+		
+	}
+	
+	public static void ButtonModel(JButton jb) {
+		
+	}
+}
