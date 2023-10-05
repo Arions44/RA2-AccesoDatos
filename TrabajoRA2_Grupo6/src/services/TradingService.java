30bee344d99ba8e0a04a6c1cde4669a1b253f5e6
javib <javib@EL-BICHO>
2023-10-05 17:12:30 +0200

    TradingService - Method to automatically update completed

--- TrabajoRA2_Grupo6/src/services/TradingService.java
+++ TrabajoRA2_Grupo6/src/services/TradingService.java
@@ -33,13 +33,27 @@ public class TradingService {
 		return id + 1;
 	}
 	
-	public static boolean insertTrading(Trading t) {
+	public static boolean insertTrading(Trading t, String transactionType) {
 		String sql = "INSERT INTO Trading VALUES("+t.getId()+", "+t.getId_product()+", "+t.getId_provider()
 		+", "+t.getAmount()+", \'"+new SimpleDateFormat("yyyy-MM-dd").format(t.getDate())+"\', \'"+t.getType()+"\');";
 		try {
 			Connection cnn = DriverManager.getConnection(AzureSql.getCnnString());
 			PreparedStatement statement = cnn.prepareStatement(sql);
 			statement.execute();
+			statement.setInt(1, t.getId());
+            statement.setInt(2, t.getId_product());
+            statement.setInt(3, t.getId_provider());
+            statement.setInt(4, t.getAmount());
+            statement.setString(5, new SimpleDateFormat("yyyy-MM-dd").format(t.getDate()));
+            statement.setString(6, transactionType); //("sell" or "buy")
+            statement.execute();
+            
+            //Here I update the stock after each transaction
+            if (transactionType.equals("buy")) {
+                updateProductStock(t.getId_product(), t.getAmount()); //Increment stock
+            } else if (transactionType.equals("sell")) {
+                updateProductStock(t.getId_product(), -t.getAmount()); //Decrement stock
+            }
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -47,32 +61,52 @@ public class TradingService {
 		}
 	}
 	
-public static ArrayList<Trading> selectTrading(String field, Object value){
-		
-		ArrayList<Trading> trades = new ArrayList<Trading>();
-		ResultSet resultSet = null;
-		String sql = "SELECT * FROM Trading";
-		if(field == null) {
-			sql += ";";
-		}else if(field.equalsIgnoreCase("id, id_provider, id_product, amount")){
-			sql += " WHERE(" + field + " = " + value +");";
-		}else {
-			sql += " WHERE(" + field + " = \'" + value +"\');";
-		}
-		
-		try {
-			Connection conn = DriverManager.getConnection(AzureSql.getCnnString());
-			Statement statement = conn.createStatement();
-			resultSet = statement.executeQuery(sql);
-			while(resultSet.next()) {
-				trades.add(new Trading(resultSet.getInt(1), resultSet.getInt(2), resultSet.getInt(3), resultSet.getInt(4), resultSet.getString(5), resultSet.getString(6)));
+	public static ArrayList<Trading> selectTrading(String field, Object value){
+			
+			ArrayList<Trading> trades = new ArrayList<Trading>();
+			ResultSet resultSet = null;
+			String sql = "SELECT * FROM Trading";
+			if(field == null) {
+				sql += ";";
+			}else if(field.equalsIgnoreCase("id, id_provider, id_product, amount")){
+				sql += " WHERE(" + field + " = " + value +");";
+			}else {
+				sql += " WHERE(" + field + " = \'" + value +"\');";
 			}
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				Connection conn = DriverManager.getConnection(AzureSql.getCnnString());
+				Statement statement = conn.createStatement();
+				resultSet = statement.executeQuery(sql);
+				while(resultSet.next()) {
+					trades.add(new Trading(resultSet.getInt(1), resultSet.getInt(2), resultSet.getInt(3), resultSet.getInt(4), resultSet.getString(5), resultSet.getString(6)));
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return trades;
 		}
-		return trades;
-	}
+	
+	
+	//Method to increment o decrement the stock of the products in the data base
+	private static void updateProductStock(int productId, int amountChange) {
+        String sql = "UPDATE Products SET stock = stock + ? WHERE id = ?";
+        try {
+            Connection cnn = DriverManager.getConnection(AzureSql.getCnnString());
+            PreparedStatement statement = cnn.prepareStatement(sql);
+            statement.setInt(1, amountChange);
+            statement.setInt(2, productId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+	
+	
+
+
 
 
 }
